d76e4f9752025bd65699b3677f1fc33238c42437
Mustafa Iram <mustafaudoy@gmail.com>
2022-11-06 02:56:39 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/profile", (req, res) => {
+  res.send("This is profile");
+});
+app.get("/alumni", (req, res) => {
+  res.send("Alumni page");
+});
+app.listen(5000, () => {
+  console.log(`Example app listening on port ${5000}`);
+});
