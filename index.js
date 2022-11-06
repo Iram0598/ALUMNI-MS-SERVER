68a956cfd7752c85f045878a7da795ce2767026a
Mustafa Iram <mustafaudoy@gmail.com>
2022-11-07 03:03:55 +0600

    MongoDB installed , environment ready

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const app = express();
+const path = require("path")
+
+const publicPath = path.join(__dirname, 'public')
 
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("", (_, res) => {
+ res.sendFile(`${publicPath}/index.html`)
 });
+
+app.get("/alumni", (_, res) => {
+  res.sendFile(`${publicPath}/alumni.html`)
+ });
 
 app.get("/profile", (req, res) => {
-  res.send("This is profile");
+  res.send(`<h1>Hello friend</h1>
+  <input type="text" placeholder="Enter text" >
+  <button type="button">Click</button> 
+  `);
 });
 app.get("/alumni", (req, res) => {
-  res.send("Alumni page");
+  res.send([
+    {
+      name: "udoy",
+      email: "udoy@gmail.com"
+    },
+    {
+      name: "bappi",
+      email: "bappi@gmail.com"
+    }
+  ]);
 });
 app.listen(5000, () => {
   console.log(`Example app listening on port ${5000}`);
