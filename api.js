ed39366dff55b250e82f7f2f42a7392870245407
Mustafa Iram <mustafaudoy@gmail.com>
2022-11-07 17:12:08 +0600

    GET and POST api checked

--- api.js
+++ api.js
@@ -2,10 +2,17 @@ const dbConnect = require("./mongo");
 const express = require("express");
 const app = express();
 
+app.use(express.json());
 app.get("/", async (req, res) => {
   let data = await dbConnect();
   data = await data.find().toArray();
   res.send(data);
+});
+
+app.post("/", async (req, res) => {
+  let data = await dbConnect();
+  let result = await data.insertOne(req.body);
+  res.send(result);
 });
 
 app.listen(5000);
