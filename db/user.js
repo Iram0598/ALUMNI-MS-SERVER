b665524c85402ce177bf7d11c21c83344e571855
Mustafa Iram <mustafaudoy@gmail.com>
2022-11-09 17:45:10 +0600

    Usert model in proggress

---
+++ db/user.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+
+    id: Number,
+    email: String,
+    password: String
+
+});
+
+module.exports = mongoose.model("users", userSchema)
