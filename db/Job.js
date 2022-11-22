4f2cee0aa7242f6667bf7a0f50756ebc8c59b09d
Mustafa Iram <mustafaudoy@gmail.com>
2022-11-22 15:30:46 +0600

    Job schema updated

--- db/Job.js
+++ db/Job.js
@@ -5,9 +5,17 @@ const jobSchema = new mongoose.Schema({
     job_title: String,
     job_des: String,
     job_photo: String,
-    eventdate: String,
-    regfee: String,
-    location: String
+    job_resp: String,
+    empt_stat: String,
+    workplace: String,
+    edu_req: String,
+    job_location: String,
+    salary: String,
+    compensation: String,
+    app_deadline: String,
+    published_date: String,
+    vacancy: String,
+   
 
 });
 
